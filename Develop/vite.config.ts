2b3741389c689e7206bdec3ec2174e13c613defb
Fortunate122 <davidsaldana122@gmail.com>
2025-03-26 21:27:43 -0700

    changes to package.json and vite.config.ts to work on render

--- Develop/vite.config.ts
+++ Develop/vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
-});
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: ['candidate-search-9bdq.onrender.com'],
+  },
+})
